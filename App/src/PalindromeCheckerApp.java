91841185a5d16c15a684ca2b7e576b753e2729c5
AntikRay <antikray2006@gmail.com>
2026-02-18 15:39:16 +0530

    Antik Added UC2-HardCodePalidrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,5 +2,9 @@
 //author Abc
 //useCase 1: Welcome page
 public class PalindroneCheckerApp {
-    public static void main(String[ ] args){
-        System. out. println("Welcome to Palindrome Checker App Management Syste
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
+    }
+
+}
