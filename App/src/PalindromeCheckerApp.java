39fc25fd53c1f4f610f361fb3689db7a6de52d0c
AntikRay <antikray2006@gmail.com>
2026-02-25 14:59:58 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,41 @@
-// version 1.0
-//author Abc
-//useCase 1: Welcome page
-public class PalindroneCheckerApp {
-    public static void main(String[ ] args){
-        System. out. println("Welcome to Palindrome Checker App Management Syste
+import java.util.Scanner;
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Create Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+
+        // Create Stack to store characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character into stack
+        for (int i = 0; i < inputString.length(); i++) {
+            stack.push(inputString.charAt(i));
+        }
+
+        // Variable to store reversed string
+        String reversedString = "";
+
+        // Pop characters from stack (LIFO order)
+        while (!stack.isEmpty()) {
+            reversedString = reversedString + stack.pop();
+        }
+
+        // Compare original and reversed string
+        if (inputString.equals(reversedString)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        // Close scanner
+        scanner.close();
+    }
+}
