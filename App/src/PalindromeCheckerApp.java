960624e7a96aac1eafd898f512707a89cdc55b14
AntikRay <antikray2006@gmail.com>
2026-02-25 15:43:01 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,42 +1,30 @@
-import java.util.Scanner;
+Palindrome Checker App
+The objective of the PalindromeChecker App is to design and implement a console-based Java application that validates whether a given string is a palindrome under different conditions, while strengthening core programming fundamentals and data structure concepts.
 
-public class UseCase9PalindromeCheckerApp {
+UC10: Case-Insensitive & Space-Ignored Palindrome
+Goal: Ignore spaces and case while checking a palindrome.
+Flow:
+Normalize string
+Apply previous logic
+Key Concepts used in UC10:
+String preprocessing
+Regular expressions
+Data Structure: String / Array
 
-    // Recursive function to check palindrome
-    public static boolean isPalindrome(String str, int start, int end) {
 
-        // Base Condition: If pointers cross or meet
-        if (start >= end) {
-            return true;
-        }
 
-        // If characters at start and end do not match
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
 
-        // Recursive call for next inner characters
-        return isPalindrome(str, start + 1, end - 1);
-    }
 
-    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
 
-        // Remove spaces and convert to lowercase (optional normalization)
-        input = input.replaceAll("\\s+", "").toLowerCase();
 
-        boolean result = isPalindrome(input, 0, input.length() - 1);
 
-        if (result) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
-        }
 
-        scanner.close();
-    }
-}
+Please refer to the code snapshot below to write your code
+
+Please refer to the hint below to write your code
+
+
+Please first compile the program using javac UseCase10PalindromeCheckerApp.java and run the Program using java UseCase10PalindromeCheckerApp as shown below
+
