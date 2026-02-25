0caac705231062ef54ba3b42c8089b041281660e
AntikRay <antikray2006@gmail.com>
2026-02-25 14:51:18 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,32 @@
-// version 1.0
-//author Abc
-//useCase 1: Welcome page
-public class PalindroneCheckerApp {
-    public static void main(String[ ] args){
-        System. out. println("Welcome to Palindrome Checker App Management Syste
+import java.util.Scanner;
+
+public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Create Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String originalString = scanner.nextLine();
+
+        // Variable to store reversed string
+        String reversedString = "";
+
+        // Reverse string using for loop
+        for (int i = originalString.length() - 1; i >= 0; i--) {
+            reversedString = reversedString + originalString.charAt(i);
+        }
+
+        // Compare original and reversed string using equals()
+        if (originalString.equals(reversedString)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        // Close scanner
+        scanner.close();
+    }
+}
